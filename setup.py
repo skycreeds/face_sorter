80f9f2d331ad39f70105b230893fffcbcadd2bc6
skycreeds <christovincent2002@gmail.com>
2024-01-15 21:15:31 +0530

    setup update

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 setup(
-    name = 'pic_sorter',
+    name = 'face_sorter',
     version = '0.1.0',
-    packages = ['pic_sorter'],
+    packages = ['face_sorter'],
     install_requires=['face_recognition','tdqm'],
-    package_dir={'pic_sorter':'pic_sorter'},
+    package_dir={'face_sorter':'face_sorter'},
     entry_points = {
         'console_scripts': [
-            'pic_sorter = pic_sorter.__main__:main'
+            'face_sorter = face_sorter.__main__:main'
         ]
     })
